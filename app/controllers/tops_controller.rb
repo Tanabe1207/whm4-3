1cae08e7873bba2158130f1ed563ebafff18c0ce
root <root@DESKTOP-FJJP9AR.localdomain>
2018-04-05 20:46:31 +0900

    NoMethodError in TopsController

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,19 @@
 class TopsController < ApplicationController
   def index
     @offer = Offer.all
+    if params[:nickname].present?
+      @find_user = User.find_by(nickname: params[:nickname])
+      @offer = @find_user.offers
+      # @offer = @offer.user.get_by_nickname params[:nickname]
+    end
+    if params[:gender].present?
+      @find_user = User.find_by(gender: params[:gender])
+      @offer = @find_user.offers
+    end
+    if params[:nationality].present?
+      @find_user = User.find_by(nationality: params[:nationality])
+      @offer = @find_user.offers
+    end
     # allの場合全て表示される？
     # offerに紐づいたuserのテーブルの中身の一部も表示させたい
   end
@@ -14,6 +27,7 @@ class TopsController < ApplicationController
       render :new
     end
   end
+
 
   # def create
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,24 @@ class User < ApplicationRecord
   has_secure_password
   mount_uploader :my_image, ImageUploader
 
-  validates :nickname, presence: true
-  validates :mail, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :nickname, presence: true, uniqueness: true
+  validates :mail, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
   #validates :my_image, presence: true
   enum gender:{男性:1, 女性:2}
+  #ユーザー名による絞込
+  scope :get_by_nickname, ->(nickname){
+    where("nickname like ?", "%#{nickname}%")
+  }
+  #性別による絞込
+  scope :get_by_gender, ->(gender){
+    where(gender: gender)
+  }
+
+  enum nationality:{Japan:1, Korea:2, Taiwan:3, HongKong:4, China:5,Australia:6,NewZealand:7,France:8,German:9,Nederland:10,Canada:11}
+
+  scope :get_by_nationality, ->(nationality){
+    where(nationality: nationality)
+  }
   #validates :birthday, presence: true
   #validates :nationality, presence: true
 
@@ -31,10 +45,10 @@ end
 # "Japan"
 # "Korea"
 # "Taiwan"
-# "Hong Kong"
+# "HongKong"
 # "China"
 # "Australia"
-# "New Zealand"
+# "NewZealand"
 # "France"
 # "German"
 # "Nederland"
