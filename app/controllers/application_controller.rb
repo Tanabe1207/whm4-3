b36d8f18cb3d03a7a7980b24a65b3718cb87b357
root <root@DESKTOP-FJJP9AR.localdomain>
2018-04-13 17:14:54 +0900

    i18n not work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  #   before_filter :set_locale
+  # # 全リンクにlocale情報をセットする
+  # def default_url_options(options={})
+  #   { :locale => I18n.locale }
+  # end
+  # # リンクの多言語化に対応する
+  # def set_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   #ここのsessionはメソッド。cookieの操作をするのがsession。session.newでつかったsession変数のことではない。
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Whm4
     config.load_defaults 5.1
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # 画像アップロードのため
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
+    config.time_zone = 'Tokyo'
+    # 多言語化のため
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
